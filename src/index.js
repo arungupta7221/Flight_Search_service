5c5d0418dafe761abf71813952e7cbce3aa71ccf
arungupta7221 <48250441+arungupta7221@users.noreply.github.com>
2022-12-13 16:11:22 +0530

    project setup for flight search service

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { PORT } = require('../src/config/serverConfig')
+const setupAndStartServer = async () => {
+  const app = express()
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.listen(PORT, () => {
+    console.log(`server has started on ${PORT}`)
+  })
+}
+
+setupAndStartServer()
