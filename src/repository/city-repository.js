d08c02eafd73e1d461a98d822e84887228f20608
arungupta7221 <48250441+arungupta7221@users.noreply.github.com>
2022-12-16 21:47:56 +0530

    city-repository updated

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -3,9 +3,12 @@ const { City } = require('../models/index')
 class CityRepository {
   async createCity({ name }) {
     try {
-      const city = await City.create({ name })
+      const city = await City.create({
+        name,
+      })
       return city
     } catch (error) {
+      console.log('something went wrong in repository layer')
       throw { error }
     }
   }
@@ -17,7 +20,32 @@ class CityRepository {
           id: cityId,
         },
       })
+      return true
     } catch (error) {
+      console.log('something went wrong in repository layer')
+      throw { error }
+    }
+  }
+  async getCity(cityId) {
+    try {
+      const city = City.findByPk(cityId)
+      return city
+    } catch (error) {
+      console.log('something went wrong in repository layer')
+      throw { error }
+    }
+  }
+
+  async updateCity(data, cityId) {
+    try {
+      const city = City.update(data, {
+        where: {
+          id: cityId,
+        },
+      })
+      return city
+    } catch (error) {
+      console.log('something went wrong in repository layer')
       throw { error }
     }
   }
