77a5524d5da980e4057528473cddfce5489f6a06
arungupta7221 <48250441+arungupta7221@users.noreply.github.com>
2022-12-18 08:48:48 +0530

    fix minor bugs in city-repo for updatecity

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -36,18 +36,24 @@ class CityRepository {
     }
   }
 
-  async updateCity(data, cityId) {
-    try {
-      const city = City.update(data, {
-        where: {
-          id: cityId,
-        },
-      })
-      return city
-    } catch (error) {
-      console.log('something went wrong in repository layer')
-      throw { error }
-    }
+  async updateCity(cityId, data) {
+    // The below approach also works but will not return updated object
+    // if we are using Pg then returning: true can be used, else not
+    // const city = await City.update(data, {
+    //     where: {
+    //         id: cityId
+    //     },
+    //
+    // });
+    // for getting updated data in mysql we use the below approach
+    const city = await City.findByPk(cityId)
+    city.name = data.name
+    await city.save()
+    return city
+  }
+  catch(error) {
+    console.log('something went wrong in repository layer')
+    throw { error }
   }
 }
 
